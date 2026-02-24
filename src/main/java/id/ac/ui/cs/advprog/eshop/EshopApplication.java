7ea64449cab8ea680724fa6bebee162f198dc665
farishuda <farishudaku@gmail.com>
2026-02-24 14:43:49 +0700

    fix issues from static analysis (2)

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SuppressWarnings("PMD.ClassWithOnlyPrivateConstructorsShouldBeFinal")
 public class EshopApplication {
 
     private EshopApplication(){
